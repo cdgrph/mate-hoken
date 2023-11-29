cc8e88f53e656aa423700160334667177070bd1c
Wataru Tamanaha <wataru@cdgrph.com>
2023-11-29 19:35:24 +0900

    トップ、メニュー、保険概要のセクションをコーディング

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,19 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'black': '#282828',
+        'white': '#F2EFE7',
+      },
+      fontFamily: {
+        'gibson': 'canada-type-gibson',
+      },
+    },
   },
-  plugins: [],
-}
+  corePlugins: {
+    fontSize: false,
+  },
+  plugins: [require("tailwindcss-fluid-type")],
+};
